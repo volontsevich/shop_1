7b21ca4d5fc5bd2a80253f7aa66c9a26061a97f3
Volontsevich <volontsevich@gmail.com>
2015-11-05 11:15:18 +0200

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,4 @@
 class HomeController < ApplicationController
 
-  def index
-
-#=    if user_signed_in?
-#=      redirect_to :controller => 'products', action: 'index'
-#=    end
-  end
-
-  def contacts
-
-  end
-
-  def delivery
-
-  end
 
 end
